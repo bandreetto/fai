2bfbf60cb57e95842a5d29e650f1f88c058c1d68
Bruno Andreetto <bandreetto@hotmail.com>
2025-06-05 22:24:45 -0300

    Refactor

--- fai/__init__.py
+++ fai/__init__.py
@@ -2,28 +2,33 @@ from fai.read import Read
 
 
 def add_flankers_to_reads(reads: list[Read], gene_dict: dict) -> list[Read]:
-    for read in reads.copy():
-        gene_sequence = gene_dict.get(read.gene_id)
+    reads_and_genes = [[read, gene_dict.get(read.gene_id)] for read in reads]
 
-        if not gene_sequence:
-            raise ValueError(f"Gene ID {read.gene_id} not found in gene_dict.")
+    if any(gene is None for _, gene in reads_and_genes):
+        raise ValueError(
+            "One or more gene IDs in reads do not match any key in gene_dict."
+        )
 
-        if len(gene_sequence) < read.gene_position + read.size - 1:
-            raise ValueError(
-                f"Gene position overflow: Gene sequence for {read.gene_id} is too short"
-                " for the read position and size."
-            )
+    return [
+        Read(
+            read.gene_id,
+            read.gene_position,
+            read.size,
+            get_flankers(read.gene_position, read.size, gene_sequence),
+        )
+        for [read, gene_sequence] in reads_and_genes
+    ]
 
-        leading_flankers_starting_index = read.gene_position - 3
-        leading_flankers = [
-            gene_sequence[leading_flankers_starting_index + i] for i in range(4)
-        ]
 
-        trailing_flankers_starting_index = leading_flankers_starting_index + read.size
-        trailing_flankers = [
-            gene_sequence[trailing_flankers_starting_index + i] for i in range(4)
-        ]
+def get_flankers(gene_position: int, gene_size: int, gene_sequence: str) -> list[str]:
+    leading_flankers_starting_index = gene_position - 3
+    leading_flankers = [
+        gene_sequence[leading_flankers_starting_index + i] for i in range(4)
+    ]
 
-        read.flankers = leading_flankers + trailing_flankers
+    trailing_flankers_starting_index = leading_flankers_starting_index + gene_size
+    trailing_flankers = [
+        gene_sequence[trailing_flankers_starting_index + i] for i in range(4)
+    ]
 
-    return reads
+    return leading_flankers + trailing_flankers
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,4 +1,4 @@
-from fai import add_flankers_to_reads
+from fai import add_flankers_to_reads, get_flankers
 from fai.read import Read
 
 
@@ -24,3 +24,17 @@ def test_add_flankers_to_read():
     for read in reads_with_flankers:
         flankers = expected_flankers[read.gene_id]
         assert read.flankers == flankers
+
+
+def test_get_flankers():
+    gene1 = "CCTCAGCCTCACCCTTACTCCTTGCCCCAGTTCATCGCCCCCGATGGGTCTAGTCACCAATCAGCCACGCGTCAGGTCAT"  # noqa: E501
+    flankers1 = ["T", "C", "A", "G", "C", "A", "T", "C"]
+    assert flankers1 == get_flankers(5, 30, gene1)
+
+    gene2 = "GCTCACTCATACTACACGAAGCAGTCGGCCGAATCCTCCGCACCCTGGGGGTCTCTCAACTCCGTGTGAAAAGTTCCTAT"  # noqa: E501
+    flankers2 = ["C", "A", "T", "A", "C", "A", "C", "C"]
+    assert flankers2 == get_flankers(10, 33, gene2)
+
+    gene3 = "CACCGCTTCCCCCTGCGAGCGGTGTCGCCAGGTTGAGCGTATGATGTCAGGTCTTTCAATATCATCCTGGAATGTATATT"  # noqa: E501
+    flankers3 = ["C", "T", "G", "C", "A", "T", "G", "A"]
+    assert flankers3 == get_flankers(15, 28, gene3)
