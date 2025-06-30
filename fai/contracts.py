2b416bec746e34b750d21a9f663b94281ae9e101
Bruno Andreetto <bandreetto@hotmail.com>
2025-06-30 18:38:33 -0300

    Add calculate delta fn

--- fai/contracts.py
+++ fai/contracts.py
@@ -1,3 +1,10 @@
+from typing import Literal
+
+
+Nucleotide = Literal["A", "C", "G", "T"]
+FlankerPosition = Literal[0, 1, 2, 3, 4, 5, 6, 7]
+
+
 class Read:
     gene_id: str
     gene_position: int
